a266d305205be5d9a0a7fb384b19f5624418c111
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2017-11-09 22:54:46 +0530

    local modifications

--- test.sh
+++
@@ -1,5 +0,0 @@
-## Test the model saved in the output folder mentioned in config.txt
-
-python test_model.py $1
-
-python postscripts/retreive.py $2
--- train.sh
+++
@@ -1,5 +0,0 @@
-#run model script
-
-output_dir=`grep outdir\ = $1| awk -F"=" '{print $2}'`
-echo $output_dir
-python run_model.py $1
