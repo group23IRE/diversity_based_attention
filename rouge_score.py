c4f221a37311a24f65065710cec5dba2b19e68f1
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2017-11-10 12:01:40 +0530

    getting best predicted sentence

--- rouge_score.py
+++ rouge_score.py
@@ -11,6 +11,9 @@ max_rouge_1 = -1.0
 max_rouge_2 = -1.0
 max_rouge_l = -1.0
 
+tsentence = ["","",""]
+psentence = ["","",""]
+
 rouge = Rouge()
 
 with open(OUTPUT_DIR + "test_final_results_plabels") as fp1, open(OUTPUT_DIR + "test_final_results_tlabels") as fp2:
@@ -29,10 +32,16 @@ with open(OUTPUT_DIR + "test_final_results_plabels") as fp1, open(OUTPUT_DIR + "
 
 			if score['rouge-1']['f'] > max_rouge_1:
 				max_rouge_1 = score['rouge-1']['f']
+				tsentence[0] = tlabel
+				psentence[0] = plabel
 			if score['rouge-2']['f'] > max_rouge_2:
 				max_rouge_2 = score['rouge-2']['f']
+				tsentence[1] = tlabel
+				psentence[1] = plabel
 			if score['rouge-l']['f'] > max_rouge_l:
 				max_rouge_l = score['rouge-l']['f']
+				tsentence[2] = tlabel
+				psentence[2] = plabel
 
 			total_queries += 1
 
@@ -41,4 +50,8 @@ with open(OUTPUT_DIR + "test_final_results_plabels") as fp1, open(OUTPUT_DIR + "
 		print("rouge-2: ", 100 * (rouge_2 / total_queries))
 		print("rouge-l: ", 100 * (rouge_l / total_queries))
 		print("max rouge scores: ",max_rouge_1, max_rouge_2, max_rouge_l)
+		print("ground truth")
+		print(tsentence)
+		print("predicted")
+		print(psentence)
 		print("Done")
